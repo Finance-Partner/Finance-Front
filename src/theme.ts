e3a61e07f2c8989295ca8f6f310bf3ed82e4c980
JunyouPark <pjy008008@gmail.com>
2024-03-19 17:05:46 +0900

    setting

---
+++ src/theme.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from "styled-components/dist/types";
+
+export const darktheme: DefaultTheme = {
+  textColor: "white",
+  backgroundColor: "#2f3640",
+};
+export const lighttheme: DefaultTheme = {
+  textColor: "black",
+  backgroundColor: "rgb(239, 239, 239)",
+};
