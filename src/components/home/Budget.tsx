19d17f4d464affd47db64960a26c8345be197d76
JunyouPark <pjy008008@gmail.com>
2024-06-04 22:25:01 +0900

    (#15)예산 페이지 구현

--- src/components/home/Budget.tsx
+++ src/components/home/Budget.tsx
@@ -8,7 +8,15 @@ const Wrapper = styled.div`
   width: 100%;
   height: 100%;
 `;
-const GridItem = styled.div`
+const GridItem1 = styled.div`
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  padding-left:30px;
+  border-radius: 20px;
+`;
+const GridItem2 = styled.div`
   background-color: white;
   display: flex;
   align-items: center;
@@ -16,28 +24,54 @@ const GridItem = styled.div`
   border-radius: 20px;
 `;
 
-const Item1 = styled(GridItem)`
+const Item1 = styled(GridItem1)`
   grid-column: 1 / 2;
   grid-row: 1 / 2;
 `;
-const Item2 = styled(GridItem)`
+const Item2 = styled(GridItem1)`
   grid-column: 2 / 3;
   grid-row: 1 / 2;
 `;
-const Item3 = styled(GridItem)`
+const Item3 = styled(GridItem2)`
   grid-column: 1 / 2;
   grid-row: 2 / 5;
 `;
-const Item4 = styled(GridItem)`
+const Item4 = styled(GridItem2)`
   grid-column: 2 / 3;
   grid-row: 2 / 5;
 `;
-
+const Title = styled.h2`
+  font-size: 15px;
+  font-weight: bold;
+  margin-bottom: 10px;
+`;
+const Price = styled.p<{ isIncome: boolean }>`
+  font-size: 45px;
+  font-weight: bold;
+  color: ${(props) => (props.isIncome ? "#1ED8AB" : "#7763F4")};
+  span {
+    font-size: 20px;
+  }
+`;
 const Budget = () => {
   return (
     <Wrapper>
-      <Item1>1행 1열</Item1>
-      <Item2>1행 2열</Item2>
+      <Item1>
+        <div>
+          <Title>이번 달 지출</Title>
+          <Price isIncome={false}>
+            450,000<span>원</span>
+          </Price>
+        </div>
+      </Item1>
+      <Item2>
+        <div>
+          <Title>예산</Title>
+          <Price isIncome={true}>
+            1,000,000<span>원</span>
+          </Price>
+        </div>
+      </Item2>
       <Item3>1행 3열</Item3>
       <Item4>1행 4열</Item4>
     </Wrapper>
