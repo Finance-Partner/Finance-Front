69f272051b84e9664d0d46834e02b77f8db3ffe9
JunyouPark <pjy008008@gmail.com>
2024-06-07 19:07:52 +0900

    (#13)내역 페이지 삭제, 수정 구현

--- src/components/home/calendar/Detail.tsx
+++ src/components/home/calendar/Detail.tsx
@@ -4,13 +4,24 @@ import axios from "axios";
 
 // Define the expected data type
 interface Transaction {
-  id: number; // Ensure the id field is present
+  id: number;
   date: string;
   amount: number;
   content: string;
   category: string;
   isIncome: string;
 }
+
+const categoryDisplayNames: { [key: string]: string } = {
+  SALARY: "월급",
+  INTEREST: "이자",
+  MEAL: "식사",
+  SHOPPING: "쇼핑",
+  CAFE_SNACK: "카페 · 간식",
+  TRANSPORT: "교통",
+  CONVSTORE_MART: "편의점 · 마트",
+  ETC: "기타",
+};
 
 const Wrapper = styled.div`
   width: 100%;
@@ -47,8 +58,8 @@ const Td = styled.td`
 const BtnTd = styled.td`
   text-align: center;
   padding-top: 5px;
-  font-size: 30px; /* Adjust size to match icons */
-  vertical-align: middle; /* Ensure vertical alignment */
+  font-size: 30px;
+  vertical-align: middle;
 `;
 
 const Button = styled.span`
@@ -58,31 +69,33 @@ const Button = styled.span`
   border: none;
 `;
 
+const EditInput = styled.input`
+  text-align: center;
+  width: 100%;
+  height: 100%;
+  font-weight: bold;
+  padding-bottom: 5px;
+  background-color: white;
+  border: none;
+  border-bottom: 1px solid black;
+`;
+const EditSelect = styled.select`
+  text-align: center;
+  width: 100%;
+  height: 100%;
+  font-weight: bold;
+  padding-bottom: 5px;
+  background-color: white;
+  border: none;
+  border-bottom: 1px solid black;
+`;
+
 const Detail = () => {
   const [data, setData] = useState<Transaction[]>([]);
+  const [editId, setEditId] = useState<number | null>(null);
+  const [editFormData, setEditFormData] = useState<Transaction | null>(null);
   const token = localStorage.getItem("token");
   const flId = 1;
-
-  const handleDelete = (historyId: number) => {
-    axios
-      .delete(`http://43.201.7.157:8080/history`, {
-        headers: {
-          Accept: "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-        params: {
-          historyId: historyId,
-        },
-      })
-      .then((response) => {
-        // Update the state to remove the deleted item
-        console.log("Delete successful:", response.data);
-        setData((prevData) => prevData.filter((item) => item.id !== historyId));
-      })
-      .catch((error) => {
-        console.error("Error deleting data:", error);
-      });
-  };
 
   useEffect(() => {
     if (token) {
@@ -100,7 +113,76 @@ const Detail = () => {
           console.error("Error fetching data:", error);
         });
     }
-  }, [token, flId]); // Add dependencies
+  }, [token, flId]);
+
+  const handleDelete = (historyId: number) => {
+    axios
+      .delete(`http://43.201.7.157:8080/history`, {
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        params: {
+          historyId: historyId,
+        },
+      })
+      .then((response) => {
+        setData((prevData) => prevData.filter((item) => item.id !== historyId));
+      })
+      .catch((error) => {
+        console.error("Error deleting data:", error);
+      });
+  };
+
+  const handleEdit = (item: Transaction) => {
+    setEditId(item.id);
+    setEditFormData({ ...item });
+  };
+
+  const handleEditChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    if (editFormData) {
+      setEditFormData({
+        ...editFormData,
+        [e.target.name]: e.target.value,
+      });
+    }
+  };
+
+  const handleSave = () => {
+    if (editFormData) {
+      const { id, date, amount, content, category, isIncome } = editFormData;
+      const [year, month, day] = date.split("-");
+      axios
+        .patch(`http://43.201.7.157:8080/history`, null, {
+          headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          params: {
+            historyId: id,
+            year: year,
+            month: month,
+            day: day,
+            amount: amount,
+            content: content,
+            category: category,
+            isIncom: isIncome,
+          },
+        })
+        .then((response) => {
+          setData((prevData) =>
+            prevData.map((item) => (item.id === id ? editFormData : item))
+          );
+          setEditId(null);
+          setEditFormData(null);
+        })
+        .catch((error) => {
+          console.error("Error updating data:", error);
+        });
+    }
+  };
 
   return (
     <>
@@ -122,33 +204,107 @@ const Detail = () => {
             <tbody>
               {data.map((item, index) => (
                 <tr key={index}>
-                  <Td
-                    style={{
-                      color: item.isIncome === "INCOME" ? "#1ED8AB" : "#7763F4",
-                    }}
-                  >
-                    {item.isIncome === "INCOME" ? "수입" : "지출"}
+                  <Td>
+                    {editId === item.id ? (
+                      <EditSelect
+                        name="isIncome"
+                        value={editFormData?.isIncome}
+                        onChange={handleEditChange}
+                      >
+                        <option value="INCOME">수입</option>
+                        <option value="EXPENDITURE">지출</option>
+                      </EditSelect>
+                    ) : (
+                      <span
+                        style={{
+                          color:
+                            item.isIncome === "INCOME" ? "#1ED8AB" : "#7763F4",
+                        }}
+                      >
+                        {item.isIncome === "INCOME" ? "수입" : "지출"}
+                      </span>
+                    )}
                   </Td>
                   <Td>
-                    {new Date(item.date).toLocaleDateString("ko-KR", {
-                      year: "numeric",
-                      month: "long",
-                      day: "numeric",
-                    })}
+                    {editId === item.id ? (
+                      <EditInput
+                        type="date"
+                        name="date"
+                        value={editFormData?.date}
+                        onChange={handleEditChange}
+                      />
+                    ) : (
+                      new Date(item.date).toLocaleDateString("ko-KR", {
+                        year: "numeric",
+                        month: "long",
+                        day: "numeric",
+                      })
+                    )}
                   </Td>
-                  <Td>{item.amount.toLocaleString()}원</Td>
-                  <Td>{item.category}</Td>
+                  <Td>
+                    {editId === item.id ? (
+                      <EditInput
+                        type="number"
+                        name="amount"
+                        value={editFormData?.amount}
+                        onChange={handleEditChange}
+                      />
+                    ) : (
+                      `${item.amount.toLocaleString()}원`
+                    )}
+                  </Td>
+                  <Td>
+                    {editId === item.id ? (
+                      <EditSelect
+                        name="category"
+                        value={editFormData?.category}
+                        onChange={handleEditChange}
+                      >
+                        {Object.entries(categoryDisplayNames).map(
+                          ([key, displayName]) => (
+                            <option key={key} value={key}>
+                              {displayName}
+                            </option>
+                          )
+                        )}
+                      </EditSelect>
+                    ) : (
+                      categoryDisplayNames[item.category]
+                    )}
+                  </Td>
                   <Td>김웹소</Td> {/* Static author name as in the example */}
-                  <Td>{item.content}</Td>
+                  <Td>
+                    {editId === item.id ? (
+                      <EditInput
+                        type="text"
+                        name="content"
+                        value={editFormData?.content}
+                        onChange={handleEditChange}
+                      />
+                    ) : (
+                      item.content
+                    )}
+                  </Td>
                   <BtnTd>
-                    <Button className="material-symbols-outlined">edit</Button>
+                    {editId === item.id ? (
+                      <Button
+                        className="material-symbols-outlined"
+                        onClick={handleSave}
+                      >
+                        save
+                      </Button>
+                    ) : (
+                      <Button
+                        className="material-symbols-outlined"
+                        onClick={() => handleEdit(item)}
+                      >
+                        edit
+                      </Button>
+                    )}
                   </BtnTd>
                   <BtnTd>
                     <Button
-                      onClick={() => {
-                        handleDelete(item.id);
-                        console.log(item.id);
-                      }}
+                      onClick={() => handleDelete(item.id)}
                       className="material-symbols-outlined"
                     >
                       delete
