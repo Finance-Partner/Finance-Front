081959208c3b2e8a552a4ddae88ad0bb9393acba
Yeonshin <yeonnim05@gmail.com>
2024-06-17 16:24:06 +0900

    (#46) 대시보드 비율 수정 및 팀원 UI 구현

--- src/components/home/DashBoard.tsx
+++ src/components/home/DashBoard.tsx
@@ -10,12 +10,13 @@ import DashHistory from "./dashboard/DashHistory";
 
 const Wrapper = styled.div`
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-template-rows: repeat(3, 1fr);
+  grid-template-columns: 1.5fr 1fr 1fr 1.5fr;
+  grid-template-rows: 1.2fr 1.4fr 1.1fr; /* Adjust the height ratios for the rows */
   gap: 10px;
   width: 100%;
   height: 100%;
 `;
+
 const GridItem = styled.div`
   background-color: white;
   display: flex;
@@ -55,13 +56,50 @@ const Item7 = styled(GridItem)`
   grid-row: 3 / 4;
 `;
 const Item8 = styled(GridItem)`
-  grid-column: 2 / 3;
+  grid-column: 2 / 4;
   grid-row: 3 / 4;
 `;
-const Item9 = styled(GridItem)`
-  grid-column: 3/4;
-  grid-row: 3/4;
-`;
+
+
+
+// const Item1 = styled(GridItem)`
+//   grid-column: 1 / 2;
+//   grid-row: 1 / 2;
+// `;
+// const Item2 = styled(GridItem)`
+//   grid-column: 2 / 3;
+//   grid-row: 1 / 2;
+// `;
+// const Item3 = styled(GridItem)`
+//   grid-column: 3 / 4;
+//   grid-row: 1 / 2;
+// `;
+// const Item4 = styled(GridItem)`
+//   grid-column: 4 / 5;
+//   grid-row: 1 / 2;
+// `;
+
+// const Item5 = styled(GridItem)`
+//   grid-column: 1 / 4;
+//   grid-row: 2 / 3;
+// `;
+// const Item6 = styled(GridItem)`
+//   grid-column: 4 / 5;
+//   grid-row: 2 / 4;
+// `;
+
+// const Item7 = styled(GridItem)`
+//   grid-column: 1 / 2;
+//   grid-row: 3 / 4;
+// `;
+// const Item8 = styled(GridItem)`
+//   grid-column: 2 / 2;
+//   grid-row: 3 / 4;
+// `;
+// const Item9 = styled(GridItem)`
+//   grid-column: 3/4;
+//   grid-row: 3/4;
+// `;
 const DashBoard = () => {
   return (
     <Wrapper>
@@ -85,11 +123,9 @@ const DashBoard = () => {
         <Teammate />
       </Item7>
       <Item8>
-        <FixIncome />
-      </Item8>
-      <Item9>
         <FixExpense />
-      </Item9>
+      </Item8>
+
     </Wrapper>
   );
 };
--- src/components/home/dashboard/Teammate.tsx
+++ src/components/home/dashboard/Teammate.tsx
@@ -3,36 +3,74 @@ import styled from "styled-components";
 import axios from "axios";
 import { useRecoilValue } from "recoil";
 import { householderIdState, selectedLedgerState } from "../../../atom";
+import profileImg from "../../../img/basicUserImg.png";
+import crownImg from "../../../img/crownImg.png";
+import moreBtn from "../../../img/moreBtn.png";
 
 const Wrapper = styled.div`
   display: flex;
   width: 100%;
   height: 100%;
   align-items: center;
-  justify-content: center;
+  margin-left: 2.5em;
 `;
+
+const NumberWrapper = styled.div`
+  margin-right: 10px;
+  display: flex;
+  align-items: flex-end;
+`;
+
 const Title = styled.h2`
-  font-size: 13px;
+  font-size: 20px;
   font-weight: bold;
   margin-bottom: 10px;
 `;
-const Number = styled.h1`
-  font-size: 50px;
+
+const Number = styled.h1<{ isFour: boolean }>`
+  font-size: 60px;
   font-weight: bold;
 `;
-const Img = styled.img`
+
+const ImgWrapper = styled.div`
+  display: flex;
+  align-items: flex-end;
+`;
+
+const Img = styled.img<{ size: number }>`
   position: relative;
   top: 10px;
-  width: 40px;
-  height: 40px;
+  width: ${(props) => props.size}px;
+  height: ${(props) => props.size}px;
   border-radius: 50%;
-  background-color: #7763f4;
   margin-left: 5px;
 `;
 
+const CrownWrapper = styled.div`
+  position: absolute;
+  top: -10px;
+  left: 55%;
+  transform: translateX(-50%);
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+const Crown = styled.img`
+  width: 20px;
+  height: 20px;
+`;
+
+
+interface TeammateInfo {
+  userId: number;
+  name: string;
+  photo: string;
+  head: boolean;
+}
+
 const Teammate = () => {
-  const [teamSize, setTeamSize] = useState(0);
-  const [userImages, setUserImages] = useState<string[]>([]);
+  const [flUserInfo, setFlUserInfo] = useState<TeammateInfo[]>([]);
   const flId = useRecoilValue(selectedLedgerState);
 
   useEffect(() => {
@@ -51,7 +89,7 @@ const Teammate = () => {
             },
           }
         );
-        setTeamSize(response.data.length);
+        setFlUserInfo(response.data);
         // Assuming you have user images, you can set them here
         // setUserImages(response.data.map(user => user.imageUrl)); // If user has imageUrl property
       } catch (error) {
@@ -67,12 +105,29 @@ const Teammate = () => {
       <div>
         <Title>팀원 명단</Title>
         <div style={{ display: "flex", alignItems: "baseline" }}>
-          <Number>{teamSize}명</Number>
-          <div style={{ display: "flex", gap: "10px", marginLeft: "10px" }}>
-            {userImages.map((src, index) => (
-              <Img key={index} src={src} />
+          <NumberWrapper>
+            <Number isFour={flUserInfo.length >= 4}>{flUserInfo.length}</Number>
+            <span style={{ fontSize: "20px", fontWeight: "bold", marginBottom:"0.5em" }}>명</span>
+          </NumberWrapper>
+
+          <ImgWrapper>
+            {flUserInfo.slice(0, 3).map((ledger, index) => (
+              <div key={index} style={{ position: "relative" }}>
+                {index === 0 && ledger.head && (
+                  <CrownWrapper>
+                    <Crown src={crownImg} alt="crown" />
+                  </CrownWrapper>
+                )}
+                <Img
+                  src={ledger.photo ? ledger.photo : profileImg}
+                  size={40 - index * 6}
+                />
+              </div>
             ))}
-          </div>
+            {flUserInfo.length >= 4 && (
+              <Img size={40} src={moreBtn} />
+            )}
+          </ImgWrapper>
         </div>
       </div>
     </Wrapper>
